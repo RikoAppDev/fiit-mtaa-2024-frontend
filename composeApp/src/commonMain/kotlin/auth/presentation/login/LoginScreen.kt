3547d39a360e667b88d5c886db1e588677c05bf5
RikoAppDev <riko.appdev@gmail.com>
2024-03-18 16:33:59 +0100

    login screen ui update

--- composeApp/src/commonMain/kotlin/auth/presentation/login/LoginScreen.kt
+++ composeApp/src/commonMain/kotlin/auth/presentation/login/LoginScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.OutlinedTextField
@@ -17,14 +18,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import auth.presentation.login.component.LoginScreenComponent
 import auth.presentation.login.component.LoginScreenEvent
+import coil3.compose.AsyncImage
 import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import grabit.composeapp.generated.resources.Res
 import grabit.composeapp.generated.resources.email
+import grabit.composeapp.generated.resources.grabit
 import grabit.composeapp.generated.resources.grabit_logo
 import grabit.composeapp.generated.resources.login_screen__create_account
 import grabit.composeapp.generated.resources.login_screen__email
@@ -36,6 +41,7 @@ import grabit.composeapp.generated.resources.password
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.resources.vectorResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import ui.theme.OnOrange
 import ui.theme.Orange
@@ -48,14 +54,12 @@ import ui.theme.Typography
 fun LoginScreen(component: LoginScreenComponent) {
     val email by component.email.subscribeAsState()
     val password by component.password.subscribeAsState()
-    val passwordHidden by component.passwordHidden.subscribeAsState()
 
     Column(modifier = Modifier.padding(40.dp), horizontalAlignment = Alignment.CenterHorizontally) {
         Spacer(modifier = Modifier.height(120.dp))
         Image(
-            painter = painterResource(Res.drawable.grabit_logo),
-            contentDescription = stringResource(Res.string.login_screen__logo),
-            modifier = Modifier.width(167.dp).height(40.dp)
+            imageVector = vectorResource(Res.drawable.grabit),
+            contentDescription = stringResource(Res.string.login_screen__logo)
         )
         Spacer(modifier = Modifier.height(48.dp))
         Text(stringResource(Res.string.login_screen__login), fontSize = 24.sp)
@@ -86,27 +90,9 @@ fun LoginScreen(component: LoginScreenComponent) {
             ),
             visualTransformation = PasswordVisualTransformation()
         )
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(passwordHidden)
-        Spacer(modifier = Modifier.height(16.dp))
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Text(
-                stringResource(Res.string.login_screen__no_account),
-                fontSize = Typography.caption.fontSize
-            )
-            Spacer(modifier = Modifier.width(12.dp))
-            Button(
-                shape = Shapes.large,
-                onClick = { component.onEvent(LoginScreenEvent.ClickRegisterButton) },
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = Orange,
-                    contentColor = OnOrange
-                )
-            ) { Text(stringResource(Res.string.login_screen__create_account)) }
-        }
         Spacer(modifier = Modifier.height(12.dp))
         Button(
-            shape = Shapes.large,
+            shape = Shapes.medium,
             onClick = { /*component.onEvent(LoginScreenEvent.ClickLoginButton)*/ },
             modifier = Modifier.fillMaxWidth(),
             colors = ButtonDefaults.buttonColors(
@@ -114,7 +100,27 @@ fun LoginScreen(component: LoginScreenComponent) {
                 contentColor = OnOrange
             )
         ) {
-            Text(stringResource(Res.string.login_screen__login), modifier = Modifier.padding(8.dp))
+            Text(
+                stringResource(Res.string.login_screen__login),
+                modifier = Modifier.padding(8.dp),
+                fontSize = Typography.button.fontSize
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                stringResource(Res.string.login_screen__no_account),
+                fontSize = Typography.caption.fontSize
+            )
+            Spacer(modifier = Modifier.width(12.dp))
+            ClickableText(
+                text = AnnotatedString(stringResource(Res.string.login_screen__create_account)),
+                onClick = { component.onEvent(LoginScreenEvent.ClickRegisterButton) },
+                style = TextStyle(
+                    color = OnOrange,
+                    fontSize = Typography.caption.fontSize
+                ),
+            )
         }
     }
 }
