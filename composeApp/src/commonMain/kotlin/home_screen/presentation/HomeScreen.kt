c817d3eb057892c894c9b0e563f88854d9c3e333
martinvanco <vancoweb.design@gmail.com>
2024-03-23 13:18:08 +0100

    Font support

---
+++ composeApp/src/commonMain/kotlin/home_screen/presentation/HomeScreen.kt
@@ -0,0 +1,10 @@
+package home_screen.presentation
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text("Wocapity")
+
+}
--- composeApp/src/commonMain/kotlin/ui/theme/Theme.kt
+++ composeApp/src/commonMain/kotlin/ui/theme/Theme.kt
@@ -2,11 +2,22 @@ package ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Typography
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.createFontFamilyResolver
+import coil3.compose.LocalPlatformContext
+import grabit.composeapp.generated.resources.Nunito_Bold
+import grabit.composeapp.generated.resources.Nunito_Regular
+import grabit.composeapp.generated.resources.Res
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.Font
 
 private val DarkColorPalette = darkColors(
     primary = Orange,
@@ -33,6 +44,15 @@ private val LightColorPalette = lightColors(
     onBackground = Color.Black,
     onSurface = Color.Black,
     */
+)
+
+@OptIn(ExperimentalResourceApi::class)
+@Composable
+private fun CustomTypography() = Typography(
+    defaultFontFamily = FontFamily(
+        Font(Res.font.Nunito_Regular, FontWeight.Normal, FontStyle.Normal),
+        Font(Res.font.Nunito_Bold, FontWeight.Bold, FontStyle.Normal)
+    ),
 )
 
 @Composable
