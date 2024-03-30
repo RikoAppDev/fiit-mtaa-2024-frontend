f04626254c1bb73407c7a8292a8b925098d48603
martinvanco <vancoweb.design@gmail.com>
2024-03-30 12:29:13 +0100

    event detail layout

--- composeApp/src/commonMain/kotlin/event_detail/presentation/event_detail_worker/EventDetailScreen.kt
+++ composeApp/src/commonMain/kotlin/event_detail/presentation/event_detail_worker/EventDetailScreen.kt
@@ -1,34 +1,29 @@
 package event_detail.presentation.event_detail_worker
 
-import account_detail.presentation.account_detail.component.AccountDetailScreenEvent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.BottomNavigation
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.ProgressIndicatorDefaults
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -36,21 +31,24 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil3.ImageLoader
@@ -59,24 +57,22 @@ import coil3.compose.LocalPlatformContext
 import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import core.presentation.components.button_primary.ButtonPrimary
 import core.presentation.components.category_chip.CategoryChip
-import core.presentation.components.filled_input.FilledInput
 import event_detail.presentation.event_detail_worker.component.EventDetailScreenComponent
 import event_detail.presentation.event_detail_worker.component.EventDetailScreenEvent
 import grabit.composeapp.generated.resources.Res
-import grabit.composeapp.generated.resources.account_detail__discard_changed
-import grabit.composeapp.generated.resources.account_detail__save_changes
-import grabit.composeapp.generated.resources.account_detail__screen_title
-import grabit.composeapp.generated.resources.account_detail__update
 import grabit.composeapp.generated.resources.capacity
 import grabit.composeapp.generated.resources.categories
-import grabit.composeapp.generated.resources.event_detail_screen__featured_image
+import grabit.composeapp.generated.resources.event_detail_screen__edit
+import grabit.composeapp.generated.resources.event_detail_screen__end_harvest
+import grabit.composeapp.generated.resources.event_detail_screen__end_harvest_notice
+import grabit.composeapp.generated.resources.event_detail_screen__sign_in_for_harvest
+import grabit.composeapp.generated.resources.event_detail_screen__signed_for_workers
+import grabit.composeapp.generated.resources.event_detail_screen__start_event
 import grabit.composeapp.generated.resources.event_detail_screen__title
+import grabit.composeapp.generated.resources.eye
 import grabit.composeapp.generated.resources.home
 import grabit.composeapp.generated.resources.location
 import grabit.composeapp.generated.resources.organizer
-import grabit.composeapp.generated.resources.phone_number
-import grabit.composeapp.generated.resources.placeholder
-import grabit.composeapp.generated.resources.placeholder_icon
 import grabit.composeapp.generated.resources.profile
 import grabit.composeapp.generated.resources.salary
 import grabit.composeapp.generated.resources.sallary
@@ -85,27 +81,31 @@ import grabit.composeapp.generated.resources.time_circle
 import grabit.composeapp.generated.resources.tooling
 import grabit.composeapp.generated.resources.tooling_provided
 import grabit.composeapp.generated.resources.tooling_required
+import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.resources.vectorResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import ui.domain.ColorVariation
-import ui.theme.LightApple
-import ui.theme.LightGrey
-import ui.theme.LightOnApple
 import ui.theme.LightOnOrange
-import ui.theme.SecondaryText
 import ui.theme.Shapes
 
-@OptIn(ExperimentalResourceApi::class, ExperimentalMaterial3Api::class)
+@OptIn(
+    ExperimentalResourceApi::class, ExperimentalMaterial3Api::class,
+    ExperimentalMaterialApi::class
+)
 @Preview
 @Composable
 fun EventDetailScreen(component: EventDetailScreenComponent) {
 
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
     val isLoading by component.isLoading.subscribeAsState()
+    var showBottomSheet by remember { mutableStateOf(false) }
+
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+
 
     Scaffold(
         topBar = {
@@ -157,10 +157,11 @@ fun EventDetailScreen(component: EventDetailScreenComponent) {
                     Box(Modifier.fillMaxWidth()) {
                         ButtonPrimary(
                             type = ColorVariation.APPLE,
-                            text = "Prihlásiť sa na zber",
+                            text = stringResource(Res.string.event_detail_screen__sign_in_for_harvest),
                             onClick = {}
                         )
                     }
+
                 }
             }
         }
@@ -311,10 +312,166 @@ fun EventDetailScreen(component: EventDetailScreenComponent) {
                                 CategoryChip("🍠 Reťkovka", color = ColorVariation.ORANGE)
                             }
                         }
+
+                        Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Text(
+                                    text = stringResource(Res.string.event_detail_screen__signed_for_workers),
+                                    style = MaterialTheme.typography.h2,
+                                    color = MaterialTheme.colors.onBackground
+                                )
+
+                                Text(
+                                    text = "4/10",
+                                    style = MaterialTheme.typography.body1,
+                                    color = MaterialTheme.colors.onBackground
+                                )
+                            }
+
+                            Column(
+                                verticalArrangement = Arrangement.spacedBy(8.dp),
+
+                                ) {
+                                listOf<String>("1", "2", "3", "4").forEach {
+                                    Row(
+                                        Modifier.fillMaxWidth().background(
+                                            MaterialTheme.colors.surface,
+                                            Shapes.medium
+                                        ).padding(
+                                            start = 12.dp,
+                                            end = 12.dp,
+                                            top = 8.dp,
+                                            bottom = 8.dp
+                                        ),
+                                        verticalAlignment = Alignment.CenterVertically,
+                                        horizontalArrangement = Arrangement.SpaceBetween
+                                    ) {
+                                        Column {
+                                            Text(
+                                                text = "Name Surname",
+                                                style = MaterialTheme.typography.h3,
+                                                color = MaterialTheme.colors.onBackground
+                                            )
+
+                                            Text(
+                                                text = "Signed at 12.4.2024 15:00",
+                                                style = MaterialTheme.typography.body2,
+                                                color = MaterialTheme.colors.secondary
+                                            )
+                                        }
+                                        IconButton(
+                                            modifier = Modifier.padding(2.dp)
+                                                .background(
+                                                    MaterialTheme.colors.surface,
+                                                    Shapes.medium
+                                                ),
+                                            onClick = {
+                                                showBottomSheet = true
+                                            },
+                                        ) {
+                                            Icon(
+                                                modifier = Modifier.size(20.dp),
+                                                imageVector = vectorResource(Res.drawable.eye),
+                                                contentDescription = null
+                                            )
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+
+                    //DEV Button layouts
+                    Column(Modifier.padding(top = 48.dp)) {
+                        Box(Modifier.fillMaxWidth()) {
+                            Row(
+                                horizontalArrangement = Arrangement.spacedBy(8.dp)
+                            ) {
+                                ButtonPrimary(
+                                    buttonModifier = Modifier.weight(1f),
+                                    type = ColorVariation.ORANGE,
+                                    text = stringResource(Res.string.event_detail_screen__edit),
+                                    onClick = {}
+                                )
+                                ButtonPrimary(
+                                    buttonModifier = Modifier.weight(1f),
+                                    type = ColorVariation.APPLE,
+                                    text = stringResource(Res.string.event_detail_screen__start_event),
+                                    onClick = {}
+                                )
+                            }
+                        }
+
+                        Box(Modifier.fillMaxWidth()) {
+                            Column(
+                                verticalArrangement = Arrangement.spacedBy(8.dp)
+                            ) {
+                                ButtonPrimary(
+                                    type = ColorVariation.CHERRY,
+                                    text = stringResource(Res.string.event_detail_screen__end_harvest),
+                                    onClick = {}
+                                )
+                                Text(
+                                    text = stringResource(Res.string.event_detail_screen__end_harvest_notice),
+                                    textAlign = TextAlign.Center,
+                                    style = MaterialTheme.typography.body2,
+                                    color = MaterialTheme.colors.secondary
+                                )
+                            }
+                        }
+                        Box(Modifier.fillMaxWidth()) {
+                            Column(
+                                verticalArrangement = Arrangement.spacedBy(8.dp)
+                            ) {
+                                Text(
+                                    text = "You cant sign as organiser",
+                                    style = MaterialTheme.typography.body1,
+                                    color = MaterialTheme.colors.error,
+                                    textAlign = TextAlign.Center
+                                )
+                            }
+                        }
+                    }
+                }
+
+                if (showBottomSheet) {
+                    ModalBottomSheet(
+                        modifier = Modifier.height(400.dp),
+                        windowInsets = WindowInsets(0, 0, 0, -2),
+                        sheetState = sheetState,
+                        onDismissRequest = { showBottomSheet = false },
+                        containerColor = MaterialTheme.colors.background,
+                        scrimColor = Color.Black.copy(alpha = 0.4f)
+
+                    ) {
+                        Column(Modifier.fillMaxWidth().padding(start = 24.dp, end = 24.dp)) {
+                            Text(
+                                "Name Surname",
+                                style = MaterialTheme.typography.h1,
+                                color = MaterialTheme.colors.onBackground
+                            )
+                            Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
+                                Text(
+                                    "Email: 1234@mail.com",
+                                    style = MaterialTheme.typography.body1,
+                                    color = MaterialTheme.colors.onBackground
+                                )
+
+                                Text(
+                                    "Phone: 0915 123 123",
+                                    style = MaterialTheme.typography.body1,
+                                    color = MaterialTheme.colors.onBackground
+                                )
+                            }
+                        }
                     }
                 }
             }
-
         }
     }
 }
